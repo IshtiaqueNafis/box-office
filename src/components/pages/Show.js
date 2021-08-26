36cb612fce58d0a861d31f2a05a809d909d253c5
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-08-25 22:03:31 -0400

    replaced use state with useReducer on the showPage

--- src/components/pages/Show.js
+++ src/components/pages/Show.js
@@ -1,31 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import {useParams} from "react-router";
 import {apiGET} from "../../misc/config";
 
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS': {
+            return {
+                isLoading: false,
+                show: action.show,
+                error: null,
+            }
+        }
+        case 'FETCH_FAILED': {
+            return {
+                ...prevState,
+                isLoading: false,
+                error: action.error
+            }
+        }
+
+        default:
+            return prevState;
+    }
+}
+
+const initialState = {
+    show: null,
+    isLoading: true,
+    error: null,
+}
+// eslint-disable-next-line consistent-return
 const Show = () => {
     const {id} = useParams() // is gained from parameters.
-    const [show, setShow] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    console.log(state)
+
     useEffect(() => {
         let isMounted = true;
 
 
         apiGET(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-            setTimeout(() => {
-                if(isMounted){
-                    setShow(results);
-                    setIsLoading(false);
-                }
 
-
-            }, 2000)
+            if (isMounted) {
+                dispatch({type: 'FETCH_SUCCESS', show: results});
+                // this will push through with FETCH_SUCCESS then set the show to based on the payload.
+            }
 
 
         }).catch(err => { // only when mounted then load it.
-            if(isMounted){
-                setError(err.message);
-                setIsLoading(false);
+            if (isMounted) {
+                dispatch({type: 'FETCH_FAILED', error: err.message})
             }
 
         })
@@ -34,12 +60,7 @@ const Show = () => {
         }
     }, [id])
 
-    if (isLoading) {
-        return <div>data is being loaded</div>
-    }
-    if (error) {
-        return <div>Error occurred {error}</div>
-    }
+    return <div>hi  </div>
 }
 export default Show;
 
